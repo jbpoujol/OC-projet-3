55c7ae8688c5acacda78a4663e0fe75611927850
Jean-Baptiste Poujol <47899256+jbpoujol@users.noreply.github.com>
2024-02-02 10:11:35 +0100

    chore: init

---
+++ src/main/java/com/openclassrooms/model/Message.java
@@ -0,0 +1,35 @@
+package com.openclassrooms.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "messages")
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "rental_id")
+    private Long rentalId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+    @Column(name = "updated_at")
+    private String updatedAt;
+    
+}
