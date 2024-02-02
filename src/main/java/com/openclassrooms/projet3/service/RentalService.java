adadbceab6ce155c2def8c93943428e0f6a0d2fb
Jean-Baptiste Poujol <47899256+jbpoujol@users.noreply.github.com>
2024-02-02 11:07:42 +0100

    feat: add rental service

---
+++ src/main/java/com/openclassrooms/projet3/service/RentalService.java
@@ -0,0 +1,58 @@
+package com.openclassrooms.projet3.service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.openclassrooms.projet3.model.Rental;
+import com.openclassrooms.projet3.repository.RentalRepository;
+
+@Service
+public class RentalService {
+
+
+    @Autowired
+    public RentalRepository rentalRepository;
+
+    public Iterable<Rental> findAllRentals() {
+        return rentalRepository.findAll();
+    }
+
+    public Optional<Rental> findRentalById(Long id) {
+        return rentalRepository.findById(id);
+    }
+
+    public Rental saveRental(Rental Rental) {
+        return rentalRepository.save(Rental);
+    }
+
+    public Rental updateRental(Long id, Rental rentalDetails) {
+        Rental rental = rentalRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Rental not found for this id :: " + id));
+    
+        rental.setName(rentalDetails.getName());
+        rental.setSurface(rentalDetails.getSurface());
+        rental.setPrice(rentalDetails.getPrice());
+        rental.setPicture(rentalDetails.getPicture());
+        rental.setDescription(rentalDetails.getDescription());
+        rental.setOwnerId(rentalDetails.getOwnerId());
+        rental.setUpdatedAt( LocalDate.now() );
+        final Rental updatedRental = rentalRepository.save(rental);
+        return updatedRental;
+    }
+    
+
+    public void deleteRental(Long id) {
+        rentalRepository.deleteById(id);
+    }
+
+    public class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message) {
+            super(message);
+        }
+    }
+    
+
+}
