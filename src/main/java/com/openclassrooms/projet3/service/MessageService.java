8b7d6a7d58244043855e214617e84bc9869662b9
Jean-Baptiste Poujol <47899256+jbpoujol@users.noreply.github.com>
2024-02-20 11:35:17 +0100

    docs: add doc

--- src/main/java/com/openclassrooms/projet3/service/MessageService.java
+++ src/main/java/com/openclassrooms/projet3/service/MessageService.java
@@ -7,32 +7,20 @@ import com.openclassrooms.projet3.model.Message;
 public interface MessageService {
 
     /**
-     * Saves a message object to the database.
-     * <p>
-     * This method takes a Message entity as input and uses the messageRepository to persist
-     * the entity to the database. It's a straightforward wrapper around the repository's save
-     * method, providing an abstraction layer that could be extended with additional logic
-     * (e.g., logging, events) in the future.
+     * Saves a message entity to the repository.
      *
-     * @param message The Message entity to be saved. Assumes the entity is valid and ready to be persisted.
+     * @param message The message entity to be saved.
      */
     void saveMessage(Message message);
 
     /**
-     * Creates and saves a new message based on the provided DTO.
+     * Creates a new message from a DTO and saves it.
      * <p>
-     * This method orchestrates the process of creating a new message from a MessageDTO, which includes
-     * looking up the associated Rental and DBUser entities based on IDs provided in the DTO. If either
-     * the rental or the user is not found, a CustomNotFoundException is thrown. Once both entities are
-     * verified to exist, a new Message entity is created, populated with the DTO and entity data, and
-     * then saved to the database.
-     * <p>
-     * This method abstracts away the complexities of the entity creation and association, ensuring that
-     * the service layer handles business logic and entity relationships.
+     * This method fetches the associated Rental and DBUser based on IDs provided in the
+     * MessageDTO. It then creates a new Message entity and saves it to the repository.
      *
-     * @param messageDTO The MessageDTO containing the data necessary to create a new message.
-     *                   This includes the IDs for the associated rental and user, as well as the message content.
-     * @throws CustomNotFoundException if either the rental or the user specified in the DTO does not exist.
+     * @param messageDTO The DTO containing the message details.
+     * @throws CustomNotFoundException if the rental or user specified in the DTO does not exist.
      */
     void createAndSaveMessage(MessageDTO messageDTO) throws CustomNotFoundException;
 }
--- src/main/java/com/openclassrooms/projet3/service/impl/MessageServiceImpl.java
+++ src/main/java/com/openclassrooms/projet3/service/impl/MessageServiceImpl.java
@@ -14,20 +14,25 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageServiceImpl implements MessageService {
+    private final MessageRepository messageRepository;
+    private final RentalService rentalService;
+    private final DBUserService dbUserService;
 
     @Autowired
-    private MessageRepository messageRepository;
+    public MessageServiceImpl(MessageRepository messageRepository,
+                              RentalService rentalService,
+                              DBUserService dbUserService) {
+        this.messageRepository = messageRepository;
+        this.rentalService = rentalService;
+        this.dbUserService = dbUserService;
+    }
 
-    @Autowired
-    private RentalService rentalService;
-
-    @Autowired
-    private DBUserService dbUserService;
-
+    @Override
     public void saveMessage(Message message) {
         messageRepository.save(message);
     }
 
+    @Override
     public void createAndSaveMessage(MessageDTO messageDTO) throws CustomNotFoundException {
 
         Rental rental = rentalService.findRentalById(messageDTO.getRental_id())
